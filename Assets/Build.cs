1d2c26c0cc642c2e841bae8fedfc07718845bc66
baegichan <46126246+baegichan@users.noreply.github.com>
2023-01-07 22:04:05 +0900

    no message

--- Assets/Build.cs
+++ Assets/Build.cs
@@ -19,7 +19,7 @@ public class Build : MonoBehaviour
         options.scenes = scenes.ToArray();
 
         // Å¸°Ù °æ·Î(ºôµå °á°ú¹°ÀÌ ¿©±â »ý¼ºµÊ)
-        string path = "C:/UnityBuild/SpaceWarship/SpaceWarship.exe";
+        string path = "C:\\UnityBuild\\SpaceWarship\\SpaceWarship.exe";
         // ºôµå Å¸°Ù
         options.target = BuildTarget.StandaloneWindows;
 
